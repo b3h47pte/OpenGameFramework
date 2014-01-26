dbe813ddea06456b3063dcd126a722d022b0ec74
b3h47pte <mike.bao@berkeley.edu>
2014-01-26 13:26:14 -0800

    Multi Cube test

---
+++ Delta Applications/Tests/GFX-MultiCube/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/michaelbao/Code/ogf/Delta Applications/Tests/GFX-MultiCube
+# Build directory: /Users/michaelbao/Code/ogf/Delta Applications/Tests/GFX-MultiCube
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
